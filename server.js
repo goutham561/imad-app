2d7e9bf9c5d435a910ee8ddf04fec33352b92fd4
goutham561 <30802141+goutham561@users.noreply.github.com>
2017-08-12 22:42:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) { ///ui/style.css' url request
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('ui/main.js', function (req, res) { ///ui/style.css' url request
+app.get('/ui/main.js', function (req, res) { ///ui/style.css' url request
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/madi.png', function (req, res) {
