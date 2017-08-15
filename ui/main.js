027c0d0a49f2eaec58530db63197b9917370b941
goutham561 <30802141+goutham561@users.noreply.github.com>
2017-08-15 21:16:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,3 +44,31 @@ ul.innerHTML = list;
    request.open('GET','http://adunurigouthamkumar.imad.hasura-app.io/submit-name?name='+name,true);
    request.send(null);
 };
+
+
+/*
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange =function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200){
+            var counter = request.responseText;
+            var span =document.getElementById('count');
+            span.innerHTML = counter.toString();
+          }
+      }
+    };
+    
+   //make the request
+   request.open('GET','http://adunurigouthamkumar.imad.hasura-app.io/counter',true);
+   request.send(null);
+   
+   var names=['name1','name2','name3','name4'];
+var list='';
+for(var i=0;i<names.length;i++) {
+list += '<li>'+names[i]+'</li>';
+}
+var ul=document.getElementById('namelist');
+ul.innerHTML = list;
+};
+   */
