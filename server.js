825e08f888de73560fd2994eecf57c910c15ffef
goutham561 <30802141+goutham561@users.noreply.github.com>
2017-08-28 20:36:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/test-db',function(req,res){
            res.status(500).send(err.toString());
        }
        else{
-           res.send(JSON.stringify(result.collumns));
+           res.send(JSON.stringify(result.row));
        }
    });
 });
