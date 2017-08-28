b172008e9ef48dc086fe7b6e3ce02fa1c8d5c70a
goutham561 <30802141+goutham561@users.noreply.github.com>
2017-08-28 22:13:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,17 +41,6 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/ui/style.css', function (req, res) { ///ui/style.css' url request
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) { ///ui/style.css' url request
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
 /*
 var names=[];
 app.get('/submit-name/:name', function (req, res) {//:name is a "parameter"
@@ -63,6 +52,7 @@ app.get('/submit-name/:name', function (req, res) {//:name is a "parameter"
     res.send(JSON.stringify(names));
 });
 */
+
 var names=[];
 app.get('/submit-name', function (req, res) {
     // '/submit-name?name=abcd' will be the url in this case
@@ -72,6 +62,18 @@ app.get('/submit-name', function (req, res) {
     //JSON Javascript object notation converting //js objects into strings
     res.send(JSON.stringify(names));
 });
+
+app.get('/ui/style.css', function (req, res) { ///ui/style.css' url request
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) { ///ui/style.css' url request
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
